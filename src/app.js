1ab78bac2ff667a0cb63ad9b226e8448f232307e
George Zhu <georgezhu211@gmail.com>
2026-04-26 18:14:55 -0500

    add presentation layer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const path = require("node:path");
 
 const pokemonRoutes = require("./modules/pokemon/routes");
 
 const app = express();
 
-app.use("/pokemon", pokemonRoutes);
+// Views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+// Routes
+app.use("/pokemon", pokemonRoutes);
 app.get("/", (req, res) => res.send("Hello, world!"));
 
 module.exports = app;
--- src/modules/pokemon/controller.js
+++ src/modules/pokemon/controller.js
@@ -1,3 +1,3 @@
 exports.index = (req, res) => {
-  res.send("index route");
+  res.render("pokemon/index");
 };
