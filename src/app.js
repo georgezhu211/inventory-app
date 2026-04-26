7045bcf70ea947dbe8568aa83b2139fc9bd920e6
George Zhu <georgezhu211@gmail.com>
2026-04-26 17:59:08 -0500

    add routing layer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const pokemonRoutes = require("./modules/pokemon/routes");
+
 const app = express();
+
+app.use("/pokemon", pokemonRoutes);
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
