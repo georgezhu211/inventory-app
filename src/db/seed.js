e59f170a39af68415bae507c113a6167d30fe54c
George Zhu <georgezhu211@gmail.com>
2026-04-29 00:08:26 -0500

    update seeding script

--- src/db/seed.js
+++ src/db/seed.js
@@ -3,9 +3,12 @@
 const { Client } = require("pg");
 
 const SQL = `
+DROP TABLE IF EXISTS pokemon;
+DROP TABLE IF EXISTS types;
+
 CREATE TABLE IF NOT EXISTS types (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR ( 255 ) 
+  name VARCHAR(255) NOT NULL
 );
 
 INSERT INTO types (name)
@@ -13,13 +16,12 @@ VALUES ('Grass'), ('Fire'), ('Water'), ('Electric'), ('Normal');
 
 CREATE TABLE IF NOT EXISTS pokemon (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR ( 255 ),
+  name VARCHAR(255) NOT NULL,
   type_id INTEGER NOT NULL REFERENCES types(id)
 );
 
 INSERT INTO pokemon (name, type_id)
 VALUES ('Bulbasaur', 1), ('Charmander', 2), ('Squirtle', 3), ('Pikachu', 4), ('Eevee', 5);
-
 `;
 
 async function main() {
@@ -28,8 +30,16 @@ async function main() {
     connectionString: process.env.DB_URL,
   });
   await client.connect();
-  await client.query(SQL);
-  await client.end();
+  await client.query("BEGIN");
+  try {
+    await client.query(SQL);
+    await client.query("COMMIT");
+  } catch (e) {
+    await client.query("ROLLBACK");
+    throw e;
+  } finally {
+    await client.end();
+  }
   console.log("done");
 }
 
